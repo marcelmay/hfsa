31c2ccc52a903cd4b356e858e8ef45fea0d4451d
Marcel May <marcel.may.de@gmail.com>
2017-07-09 20:29:18 +0200

    Improved tests

--- lib/src/test/java/de/m3y/hadoop/hdfs/hfsa/core/FSImageLoaderTest.java
+++ lib/src/test/java/de/m3y/hadoop/hdfs/hfsa/core/FSImageLoaderTest.java
@@ -4,10 +4,7 @@ package de.m3y.hadoop.hdfs.hfsa.core;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.apache.hadoop.fs.permission.PermissionStatus;
@@ -17,6 +14,29 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * fsi_small.img test data content:
+ * <pre>
+ * mm   supergroup          0  /test1
+ * mm   supergroup          0  /test2
+ * mm   supergroup          0  /test3
+ * mm   supergroup          0  /test3/foo
+ * mm   supergroup          0  /test3/foo/bar
+ * mm   nobody       20971520  /test3/foo/bar/test_20MiB.img
+ * mm   supergroup    2097152  /test3/foo/bar/test_2MiB.img
+ * mm   supergroup   41943040  /test3/foo/bar/test_40MiB.img
+ * mm   supergroup    4145152  /test3/foo/bar/test_4MiB.img
+ * mm   supergroup    5181440  /test3/foo/bar/test_5MiB.img
+ * mm   supergroup   83886080  /test3/foo/bar/test_80MiB.img
+ * root root             1024  /test3/foo/test_1KiB.img
+ * mm   supergroup   20971520  /test3/foo/test_20MiB.img
+ * mm   supergroup    1048576  /test3/test.img
+ * foo  nobody      167772160  /test3/test_160MiB.img
+ * mm   supergroup       2048  /test_2KiB.img
+ * mm   supergroup          0  /user
+ * mm   supergroup          0  /user/mm
+ * </pre>
+ */
 public class FSImageLoaderTest {
     private FSImageLoader loader;
 
@@ -36,10 +56,12 @@ public class FSImageLoaderTest {
     @Test
     public void testLoadAndVisit() throws IOException {
         Set<String> paths = new HashSet<>();
+        Set<String> files = new HashSet<>();
 
         loader.visit(new FsVisitor() {
             @Override
             public void onFile(FsImageProto.INodeSection.INode inode, String path) {
+                files.add(("/".equals(path) ? path : path + '/') + inode.getName().toStringUtf8());
                 paths.add(path);
                 FsImageProto.INodeSection.INodeFile f = inode.getFile();
 
@@ -70,13 +92,30 @@ public class FSImageLoaderTest {
         assertEquals(3, userNames.size());
         assertEquals(3, groupNames.size());
         assertEquals(7, sumDirs);
-        assertEquals(10, sumFiles);
+        assertEquals(11, sumFiles);
         assertEquals(0, sumSymLinks);
-        assertEquals(348017664L, sumSize);
+        assertEquals(348019712L, sumSize);
+
         Set<String> expectedPaths = new HashSet<>(Arrays.asList(
-                "/test1", "/test2", "/test3", "/test3/foo", "/test3/foo/bar", "/user", "/user/mm"));
+                "/", "/test1", "/test2", "/test3", "/test3/foo", "/test3/foo/bar", "/user", "/user/mm"));
         assertTrue(paths.containsAll(expectedPaths));
-        assertTrue(expectedPaths.containsAll(paths));
+        assertEquals(expectedPaths.size(), paths.size());
+
+        Set<String> expectedFiles = new HashSet<>(Arrays.asList(
+                "/test_2KiB.img",
+                "/test3/test.img",
+                "/test3/test_160MiB.img",
+                "/test3/foo/test_1KiB.img",
+                "/test3/foo/test_20MiB.img",
+                "/test3/foo/bar/test_20MiB.img",
+                "/test3/foo/bar/test_2MiB.img",
+                "/test3/foo/bar/test_40MiB.img",
+                "/test3/foo/bar/test_4MiB.img",
+                "/test3/foo/bar/test_5MiB.img",
+                "/test3/foo/bar/test_80MiB.img"
+        ));
+        assertEquals(expectedFiles.size(),files.size());
+        assertTrue(expectedFiles.containsAll(files));
     }
 
     @Test
@@ -89,8 +128,8 @@ public class FSImageLoaderTest {
 
     @Test
     public void testGetFileINodesInDirectory() throws IOException {
-        // Empty directory
-        List<FsImageProto.INodeSection.INode> files = loader.getFileINodesInDirectory("/");
+        // Directory with no files but another directory
+        List<FsImageProto.INodeSection.INode> files = loader.getFileINodesInDirectory("/user");
         assertEquals(0, files.size());
 
         // Directory with two files
@@ -99,6 +138,10 @@ public class FSImageLoaderTest {
         final List<String> fileNames = files.stream().map((n) -> n.getName().toStringUtf8()).collect(Collectors.toList());
         assertTrue(fileNames.contains("test.img"));
         assertTrue(fileNames.contains("test_160MiB.img"));
+
+        // Root has a single file
+        files = loader.getFileINodesInDirectory("/");
+        assertEquals(1, files.size());
 
         // Invalid directory
         try {
@@ -136,7 +179,6 @@ public class FSImageLoaderTest {
 
             @Override
             public void onDirectory(FsImageProto.INodeSection.INode inode, String path) {
-                System.out.println(path + " : " + inode.getName().toStringUtf8());
                 paths.add(("/".equals(path) ? path : path + '/') + inode.getName().toStringUtf8());
                 FsImageProto.INodeSection.INodeDirectory d = inode.getDirectory();
                 PermissionStatus p = loader.getPermissionStatus(d.getPermission());
