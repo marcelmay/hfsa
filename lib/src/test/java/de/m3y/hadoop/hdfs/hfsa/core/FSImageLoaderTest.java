674ac1d0ff0f92cc43bfaaea8e3c7bd0404c8bf1
Marcel May <marcel.may.de@gmail.com>
2017-09-13 22:03:58 +0200

    Extended loader test with replication

--- lib/src/test/java/de/m3y/hadoop/hdfs/hfsa/core/FSImageLoaderTest.java
+++ lib/src/test/java/de/m3y/hadoop/hdfs/hfsa/core/FSImageLoaderTest.java
@@ -4,7 +4,10 @@ package de.m3y.hadoop.hdfs.hfsa.core;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.hadoop.fs.permission.PermissionStatus;
@@ -18,25 +21,39 @@ import static org.junit.Assert.*;
 
 /**
  * fsi_small.img test data content:
+ * <p>
+ * perms / blocks / user / group /     size / modified      / full path
  * <pre>
- * mm   supergroup          0  /test1
- * mm   supergroup          0  /test2
- * mm   supergroup          0  /test3
- * mm   supergroup          0  /test3/foo
- * mm   supergroup          0  /test3/foo/bar
- * mm   nobody       20971520  /test3/foo/bar/test_20MiB.img
- * mm   supergroup    2097152  /test3/foo/bar/test_2MiB.img
- * mm   supergroup   41943040  /test3/foo/bar/test_40MiB.img
- * mm   supergroup    4145152  /test3/foo/bar/test_4MiB.img
- * mm   supergroup    5181440  /test3/foo/bar/test_5MiB.img
- * mm   supergroup   83886080  /test3/foo/bar/test_80MiB.img
- * root root             1024  /test3/foo/test_1KiB.img
- * mm   supergroup   20971520  /test3/foo/test_20MiB.img
- * mm   supergroup    1048576  /test3/test.img
- * foo  nobody      167772160  /test3/test_160MiB.img
- * mm   supergroup       2048  /test_2KiB.img
- * mm   supergroup          0  /user
- * mm   supergroup          0  /user/mm
+ * drwxr-xr-x   - mm   supergroup          0 2017-07-22 09:58 /datalake
+ * drwxr-xr-x   - mm   supergroup          0 2017-07-22 09:57 /datalake/asset1
+ * drwxr-xr-x   - mm   supergroup          0 2017-07-22 10:01 /datalake/asset2
+ * -rw-r--r--   1 mm   supergroup       1024 2017-07-22 10:00 /datalake/asset2/test_1KiB.img
+ * -rw-r--r--   1 mm   supergroup    2097152 2017-07-22 10:01 /datalake/asset2/test_2MiB.img
+ * drwxr-xr-x   - mm   supergroup          0 2017-07-22 10:01 /datalake/asset3
+ * drwxr-xr-x   - mm   supergroup          0 2017-07-22 10:01 /datalake/asset3/subasset1
+ * -rw-r--r--   1 mm   supergroup    2097152 2017-07-22 10:01 /datalake/asset3/subasset1/test_2MiB.img
+ * drwxr-xr-x   - mm   supergroup          0 2017-07-22 10:01 /datalake/asset3/subasset2
+ * -rw-r--r--   1 mm   supergroup    2097152 2017-07-22 10:01 /datalake/asset3/subasset2/test_2MiB.img
+ * -rw-r--r--   1 mm   supergroup    2097152 2017-07-22 10:01 /datalake/asset3/test_2MiB.img
+ * drwxr-xr-x   - mm   supergroup          0 2017-06-17 23:03 /test1
+ * drwxr-xr-x   - mm   supergroup          0 2017-06-17 23:03 /test2
+ * drwxr-xr-x   - mm   supergroup          0 2017-06-17 23:25 /test3
+ * drwxr-xr-x   - mm   supergroup          0 2017-06-17 23:11 /test3/foo
+ * drwxr-xr-x   - mm   supergroup          0 2017-06-17 23:25 /test3/foo/bar
+ * -rw-r--r--   1 mm   nobody       20971520 2017-06-17 23:13 /test3/foo/bar/test_20MiB.img
+ * -rw-r--r--   1 mm   supergroup    2097152 2017-06-17 23:10 /test3/foo/bar/test_2MiB.img
+ * -rw-r--r--   1 mm   supergroup   41943040 2017-06-17 23:25 /test3/foo/bar/test_40MiB.img
+ * -rw-r--r--   1 mm   supergroup    4145152 2017-06-17 23:10 /test3/foo/bar/test_4MiB.img
+ * -rw-r--r--   1 mm   supergroup    5181440 2017-06-17 23:10 /test3/foo/bar/test_5MiB.img
+ * -rw-r--r--   1 mm   supergroup   83886080 2017-06-17 23:25 /test3/foo/bar/test_80MiB.img
+ * -rw-r--r--   1 root root             1024 2017-06-17 23:09 /test3/foo/test_1KiB.img
+ * -rw-r--r--   1 mm   supergroup   20971520 2017-06-17 23:11 /test3/foo/test_20MiB.img
+ * -rw-r--r--   1 mm   supergroup    1048576 2017-06-17 23:07 /test3/test.img
+ * -rw-r--r--   1 foo  nobody      167772160 2017-06-17 23:25 /test3/test_160MiB.img
+ * -rw-r--r--   1 mm   supergroup       2048 2017-07-08 08:00 /test_2KiB.img
+ * drwxr-xr-x   - mm   supergroup          0 2017-06-17 23:04 /user
+ * drwxr-xr-x   - mm   supergroup          0 2017-06-17 23:04 /user/mm
+ *
  * </pre>
  */
 public class FSImageLoaderTest {
@@ -69,7 +86,6 @@ public class FSImageLoaderTest {
                 files.add(fileName);
                 paths.add(path);
                 FsImageProto.INodeSection.INodeFile f = inode.getFile();
-
                 PermissionStatus p = loader.getPermissionStatus(f.getPermission());
                 groupNames.add(p.getGroupName());
                 userNames.add(p.getUserName());
@@ -98,13 +114,15 @@ public class FSImageLoaderTest {
 
         assertEquals(3, userNames.size());
         assertEquals(3, groupNames.size());
-        assertEquals(8, sumDirs);
-        assertEquals(11, sumFiles);
+        assertEquals(14, sumDirs);
+        assertEquals(16, sumFiles);
         assertEquals(0, sumSymLinks);
-        assertEquals(348019712L, sumSize);
+        assertEquals(356409344L, sumSize);
 
         Set<String> expectedPaths = new HashSet<>(Arrays.asList(
-                "/", "/test1", "/test2", "/test3", "/test3/foo", "/test3/foo/bar", "/user", "/user/mm"));
+                "/", "/test1", "/test2", "/test3", "/test3/foo", "/test3/foo/bar", "/user", "/user/mm",
+                "/datalake", "/datalake/asset1", "/datalake/asset2",
+                "/datalake/asset3", "/datalake/asset3/subasset1", "/datalake/asset3/subasset2"));
         assertTrue(paths.containsAll(expectedPaths));
         assertEquals(expectedPaths.size(), paths.size());
 
@@ -119,10 +137,19 @@ public class FSImageLoaderTest {
                 "/test3/foo/bar/test_40MiB.img",
                 "/test3/foo/bar/test_4MiB.img",
                 "/test3/foo/bar/test_5MiB.img",
-                "/test3/foo/bar/test_80MiB.img"
+                "/test3/foo/bar/test_80MiB.img",
+                "/datalake/asset2/test_1KiB.img",
+                "/datalake/asset2/test_2MiB.img",
+                "/datalake/asset3/subasset1/test_2MiB.img",
+                "/datalake/asset3/subasset2/test_2MiB.img",
+                "/datalake/asset3/test_2MiB.img"
         ));
-        assertEquals(expectedFiles.size(),files.size());
+        assertEquals(expectedFiles.size(), files.size());
         assertTrue(expectedFiles.containsAll(files));
+
+        assertEquals(1, loader.getINodeFromPath("/test3/foo/bar/test_40MiB.img").getFile().getReplication());
+        assertEquals(3, loader.getINodeFromPath("/test3/foo/bar/test_80MiB.img").getFile().getReplication());
+        assertEquals(5, loader.getINodeFromPath("/test3/foo/bar/test_4MiB.img").getFile().getReplication());
     }
 
     @Test
@@ -159,7 +186,7 @@ public class FSImageLoaderTest {
                 paths.add(path);
                 sumSymLinks++;
             }
-        },"/test3");
+        }, "/test3");
 
         assertEquals(3, userNames.size());
         assertEquals(3, groupNames.size());
@@ -169,7 +196,7 @@ public class FSImageLoaderTest {
         assertEquals(348017664L, sumSize);
 
         Set<String> expectedPaths = new HashSet<>(Arrays.asList(
-                 "/test3", "/test3/foo", "/test3/foo/bar"));
+                "/test3", "/test3/foo", "/test3/foo/bar"));
         assertTrue(paths.containsAll(expectedPaths));
         assertEquals(expectedPaths.size(), paths.size());
 
@@ -185,8 +212,9 @@ public class FSImageLoaderTest {
                 "/test3/foo/bar/test_5MiB.img",
                 "/test3/foo/bar/test_80MiB.img"
         ));
-        assertEquals(expectedFiles.size(),files.size());
+        assertEquals(expectedFiles.size(), files.size());
         assertTrue(expectedFiles.containsAll(files));
+//        loader.getINodeFromPath("")
     }
 
     @Test
@@ -212,7 +240,7 @@ public class FSImageLoaderTest {
     @Test
     public void testGetChildPaths() throws IOException {
         List<String> childPaths = loader.getChildPaths("/");
-        List<String> expectedChildPaths = Arrays.asList("/user", "/test1", "/test2", "/test3");
+        List<String> expectedChildPaths = Arrays.asList("/user", "/test1", "/test2", "/test3", "/datalake");
         assertTrue(expectedChildPaths.containsAll(childPaths));
         assertTrue(childPaths.containsAll(expectedChildPaths));
     }
@@ -279,7 +307,7 @@ public class FSImageLoaderTest {
         try {
             assertFalse(loader.hasChildren("/test3/nonexistent/path"));
             fail("Expected FileNotFoundException");
-        } catch(FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             // Expected
         }
     }
