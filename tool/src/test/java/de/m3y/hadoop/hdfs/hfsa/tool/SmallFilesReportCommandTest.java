6ac944537d86683e0c22244b3ea602a25de03e6c
Marcel May <marcel.may.de@gmail.com>
2020-11-22 19:57:42 +0100

    Fix locale dependant decimal separator handling

--- tool/src/test/java/de/m3y/hadoop/hdfs/hfsa/tool/SmallFilesReportCommandTest.java
+++ tool/src/test/java/de/m3y/hadoop/hdfs/hfsa/tool/SmallFilesReportCommandTest.java
@@ -3,12 +3,15 @@ package de.m3y.hadoop.hdfs.hfsa.tool;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.PrintStream;
+import java.text.DecimalFormatSymbols;
 
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SmallFilesReportCommandTest {
+    static final char DECIMAL_SEPARATOR = DecimalFormatSymbols.getInstance().getDecimalSeparator();
+
     @Test
     public void testRun() {
         SmallFilesReportCommand command = new SmallFilesReportCommand();
@@ -19,33 +22,34 @@ public class SmallFilesReportCommandTest {
             command.mainCommand.err = command.mainCommand.out;
             command.mainCommand.fsImageFile = new File("src/test/resources/fsi_small.img");
             command.run();
+            final String expected = "\n" +
+                    "Small files report (< 2 MiB)\n" +
+                    "\n" +
+                    "Overall small files : 3\n" +
+                    "\n" +
+                    "#Small files  | Path (top 10) \n" +
+                    "------------------------------\n" +
+                    "            3 | /\n" +
+                    "            2 | /test3\n" +
+                    "            1 | /test3/foo\n" +
+                    "\n" +
+                    "Username | #Small files | %\n" +
+                    "------------------------------------\n" +
+                    "mm       |            2 | 66.7%\n" +
+                    "root     |            1 | 33.3%\n" +
+                    "\n" +
+                    "Username | Small files hotspots (top 10 count/path)\n" +
+                    "---------------------------------------------------\n" +
+                    "mm       |            2 | /\n" +
+                    "         |            1 | /test3\n" +
+                    "---------------------------------------------------\n" +
+                    "root     |            1 | /\n" +
+                    "         |            1 | /test3\n" +
+                    "         |            1 | /test3/foo\n" +
+                    "---------------------------------------------------\n";
+
             assertThat(byteArrayOutputStream.toString())
-                    .isEqualTo("\n" +
-                            "Small files report (< 2 MiB)\n" +
-                            "\n" +
-                            "Overall small files : 3\n" +
-                            "\n" +
-                            "#Small files  | Path (top 10) \n" +
-                            "------------------------------\n" +
-                            "            3 | /\n" +
-                            "            2 | /test3\n" +
-                            "            1 | /test3/foo\n" +
-                            "\n" +
-                            "Username | #Small files | %\n" +
-                            "------------------------------------\n" +
-                            "mm       |            2 | 66.7%\n" +
-                            "root     |            1 | 33.3%\n" +
-                            "\n" +
-                            "Username | Small files hotspots (top 10 count/path)\n" +
-                            "---------------------------------------------------\n" +
-                            "mm       |            2 | /\n" +
-                            "         |            1 | /test3\n" +
-                            "---------------------------------------------------\n" +
-                            "root     |            1 | /\n" +
-                            "         |            1 | /test3\n" +
-                            "         |            1 | /test3/foo\n" +
-                            "---------------------------------------------------\n"
-                    );
+                    .isEqualTo(expected.replace('.', DECIMAL_SEPARATOR));
         }
     }
 
@@ -63,29 +67,29 @@ public class SmallFilesReportCommandTest {
 
             command.run();
 
+            final String expected = "\n" +
+                    "Small files report (< 2 MiB)\n" +
+                    "\n" +
+                    "Overall small files         : 3\n" +
+                    "User (filtered) small files : 2\n" +
+                    "\n" +
+                    "#Small files  | Path (top 10) \n" +
+                    "------------------------------\n" +
+                    "            3 | /\n" +
+                    "            2 | /test3\n" +
+                    "            1 | /test3/foo\n" +
+                    "\n" +
+                    "Username | #Small files | %\n" +
+                    "------------------------------------\n" +
+                    "mm       |            2 | 66.7%\n" +
+                    "\n" +
+                    "Username | Small files hotspots (top 10 count/path)\n" +
+                    "---------------------------------------------------\n" +
+                    "mm       |            2 | /\n" +
+                    "         |            1 | /test3\n" +
+                    "---------------------------------------------------\n";
             assertThat(byteArrayOutputStream.toString())
-                    .isEqualTo("\n" +
-                            "Small files report (< 2 MiB)\n" +
-                            "\n" +
-                            "Overall small files         : 3\n" +
-                            "User (filtered) small files : 2\n" +
-                            "\n" +
-                            "#Small files  | Path (top 10) \n" +
-                            "------------------------------\n" +
-                            "            3 | /\n" +
-                            "            2 | /test3\n" +
-                            "            1 | /test3/foo\n" +
-                            "\n" +
-                            "Username | #Small files | %\n" +
-                            "------------------------------------\n" +
-                            "mm       |            2 | 66.7%\n" +
-                            "\n" +
-                            "Username | Small files hotspots (top 10 count/path)\n" +
-                            "---------------------------------------------------\n" +
-                            "mm       |            2 | /\n" +
-                            "         |            1 | /test3\n" +
-                            "---------------------------------------------------\n"
-                    );
+                    .isEqualTo(expected.replace('.', DECIMAL_SEPARATOR) );
         }
     }
 }
